f975b5a9cb77bcf49b5191488d82759795d05fd5
onuk_scotia <wanosyan@gmail.com>
2022-10-23 03:20:53 +0900

    1/5

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@
 window.alert('いらっしゃいませー')
 
 const lottery = () => {
-  let flag = Math.floor(Math.random() * 2);
-  if(flag == 0){
+  let flag = Math.floor(Math.random() * 5);
+  if(flag <= 3){
     console.log(flag);
     document.getElementById('result').textContent = '残念...ハズレです';
   }
